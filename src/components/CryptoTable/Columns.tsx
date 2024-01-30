8dcfce3d78455f179d6a216f9f5574cbd98f2f7b
Piotr Kasperek <piotr.kasperek92@gmail.com>
2024-01-30 15:18:09 +0100

    BOAR-56 make it more clean

--- src/components/CryptoTable/Columns.tsx
+++ src/components/CryptoTable/Columns.tsx
@@ -17,47 +17,50 @@ export type CryptoTable = {
   verified: boolean;
   img: string;
 };
+const formatCurrency = (value: number) =>
+  new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
+    value
+  );
+
+const formatPercentage = (value: number) =>
+  new Intl.NumberFormat("en-US", {
+    style: "percent",
+    signDisplay: "always",
+    minimumFractionDigits: 2,
+  }).format(value);
+
+const generateDefaultHeader =
+  (key: string) =>
+  ({ column }: { column: Column<CryptoTable, unknown> }) => (
+    <Button
+      className="bg-transparent hover:bg-transparent hover:shadow-transparent p-0"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    >
+      {key.charAt(0).toUpperCase() + key.slice(1)}
+      <LuArrowUpDown className="ml-2" size={12} />
+    </Button>
+  );
 
 export function generateColumns(data: CryptoTable): ColumnDef<CryptoTable>[] {
   const keys = Object.keys(data);
-  const defaultHeader =
-    (key: string) =>
-    ({ column }: { column: Column<CryptoTable, unknown> }) => (
-      <Button
-        className="bg-transparent hover:bg-transparent hover:shadow-transparent p-0"
-        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-      >
-        {key.charAt(0).toUpperCase() + key.slice(1)}
-        <LuArrowUpDown className="ml-2" size={12} />
-      </Button>
-    );
-
-  const defaultCell =
-    (key: string) =>
-    ({ row }: { row: Row<CryptoTable & Record<string, unknown>> }) => (
-      <span>{String(row.original[key])}</span>
-    );
 
   return keys.reduce((acc: ColumnDef<CryptoTable>[], key) => {
-    if (key === "img" || key === "symbol") {
-      return acc;
-    }
+    if (key === "img" || key === "symbol") return acc;
 
     const column: ColumnDef<CryptoTable> = {
       accessorKey: key,
-      header: defaultHeader(key),
-      cell: defaultCell(key),
+      header: generateDefaultHeader(key),
+      cell: ({ row }: { row: Row<CryptoTable> }) => {
+        const value = row.original[key as keyof CryptoTable];
+        return <span>{String(value)}</span>;
+      },
     };
 
     switch (key) {
       case "verified":
-        column.cell = ({ row }: { row: Row<CryptoTable> }) => (
+        column.cell = ({ row }) => (
           <div className="flex items-center gap-2">
-            {row.original[key] ? (
-              <FaStar fill="yellow" size={18} />
-            ) : (
-              <FaStar className="opacity-50" size={18} />
-            )}
+            <FaStar fill={row.original[key] ? "yellow" : "gray"} size={18} />
             <span className="text-white">
               {row.original[key] ? "Verified" : "Not Verified"}
             </span>
@@ -65,26 +68,14 @@ export function generateColumns(data: CryptoTable): ColumnDef<CryptoTable>[] {
         );
         break;
       case "id":
-        column.header = ({
-          column,
-        }: {
-          column: Column<CryptoTable, unknown>;
-        }) => (
-          <Button
-            className="bg-transparent hover:bg-transparent hover:shadow-transparent p-0"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            #
-            <LuArrowUpDown className="ml-2" size={12} />
-          </Button>
-        );
+        column.header = generateDefaultHeader("#");
         break;
       case "name":
-        column.cell = ({ row }: { row: Row<CryptoTable> }) => (
+        column.cell = ({ row }) => (
           <div className="flex items-center gap-2">
             <img src={row.original.img} alt="" className="w-5 h-5" />
-            <span>{row.original[key]}</span>
-            <span className=" text-accent-gray text-xs">
+            <span>{row.original.name}</span>
+            <span className="text-accent-gray text-xs">
               {row.original.symbol}
             </span>
           </div>
@@ -93,33 +84,19 @@ export function generateColumns(data: CryptoTable): ColumnDef<CryptoTable>[] {
       case "volume24h":
       case "marketCap":
       case "price":
-        column.cell = ({ row }: { row: Row<CryptoTable> }) => (
-          <span>
-            {new Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD",
-            }).format(row.original[key])}
-          </span>
+        column.cell = ({ row }) => (
+          <span>{formatCurrency(row.original[key])}</span>
         );
         break;
       case "change1h":
       case "change24h":
       case "change7d":
-        column.cell = ({ row }: { row: Row<CryptoTable> }) => {
-          const amount = row.original[key];
-          const formatted = new Intl.NumberFormat("en-US", {
-            style: "percent",
-            signDisplay: "always",
-            minimumFractionDigits: 2,
-          }).format(amount);
-
+        column.cell = ({ row }) => {
+          const className =
+            row.original[key] < 0 ? "text-red-500" : "text-green-500";
           return (
-            <span
-              className={
-                row.original[key] < 0 ? "text-red-500" : "text-green-500"
-              }
-            >
-              {formatted}
+            <span className={className}>
+              {formatPercentage(row.original[key])}
             </span>
           );
         };
