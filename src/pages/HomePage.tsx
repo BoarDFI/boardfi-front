ef52b380b0f4893c64b7581d7e20e33957ee59db
Marcin3232 <marcin323239@gmail.com>
2024-01-31 21:32:53 +0100

    create card template

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import Card from "@/components/shared/Card";
+
 function HomePage() {
   return (
     <>
-      <h1 className="text-3xl mt-10">Hello BoardFI</h1>
+      <h1 className="text-3xl mt-10"></h1>
+      <Card title="Hot Cryptos" src="https://beeg.com/"></Card>
+      {/* <Card></Card> */}
     </>
   );
 }
