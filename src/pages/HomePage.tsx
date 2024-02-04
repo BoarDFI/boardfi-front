a493f5beaf78d4b50e94e6b6c6bd9d59937b4c7c
Marcin3232 <marcin323239@gmail.com>
2024-02-04 19:58:33 +0100

    v3

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,7 @@
-import Card from "@/components/shared/Card";
-
 function HomePage() {
   return (
     <>
       <h1 className="text-3xl mt-10"></h1>
-      <Card title="Hot Cryptos" src="https://beeg.com/"></Card>
-      {/* <Card></Card> */}
     </>
   );
 }
