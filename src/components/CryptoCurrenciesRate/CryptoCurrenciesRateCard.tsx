319d3f76f896d36ae3e2b06db0d3b7a29c03f1f9
Piotr Kasperek <piotr.kasperek92@gmail.com>
2024-03-18 17:15:48 +0100

    BOAR-74 simplify component CryptocurrenciesRateCards

--- src/components/CryptoCurrenciesRate/CryptoCurrenciesRateCard.tsx
+++ src/components/CryptoCurrenciesRate/CryptoCurrenciesRateCard.tsx
@@ -1,8 +1,6 @@
-import Card from "@/components/shared/Card";
-
 import { useEffect, useState } from "react";
+import Card from "@/components/shared/Card";
 import { CryptoCurrenciesRateViewType } from "@/types/CryptoCurrenciesRate.type";
-
 import { IconEnum } from "@/enums";
 import { mockCryptoCurrenciesRateViews } from "@/constants";
 
@@ -15,32 +13,31 @@ const CryptocurrenciesRateCards = () => {
     setRecentlyAdded(mockCryptoCurrenciesRateViews);
   }, []);
 
+  const renderCard = (
+    title: string,
+    icon: IconEnum,
+    cryptoCurrenciesRateViews: CryptoCurrenciesRateViewType[]
+  ) => (
+    <div
+      className={`row-span-1 ${icon === IconEnum.FLAME ? "mr-5 max-md:mr-0" : "ml-5 max-md:ml-0 max-md:mt-14"}`}
+    >
+      <Card
+        cryptoCurrenciesRateCardView={{
+          title,
+          href: "",
+          icon,
+          cryptoCurrenciesRateViews: cryptoCurrenciesRateViews,
+          enableBestResult: true,
+        }}
+        showBestResult={true}
+      />
+    </div>
+  );
+
   return (
     <div className="grid grid-cols-2 max-md:grid-cols-1 gap-2 mb-20">
-      <div className="row-span-1 mr-5 max-md:mr-0">
-        <Card
-          cryptoCurrenciesRateCardView={{
-            title: "Hot Cryptos",
-            href: "",
-            icon: IconEnum.FLAME,
-            cryptoCurrenciesRateViews: recentlyAdded,
-            enableBestResult: true,
-          }}
-          showBestResult={true}
-        ></Card>
-      </div>
-      <div className="row-span-1 ml-5 max-md:ml-0 max-md:mt-14">
-        <Card
-          cryptoCurrenciesRateCardView={{
-            title: "Hot Cryptos",
-            href: "",
-            icon: IconEnum.HOURGLASS,
-            cryptoCurrenciesRateViews: recentlyAdded,
-            enableBestResult: true,
-          }}
-          showBestResult={true}
-        ></Card>
-      </div>
+      {renderCard("Hot Cryptos", IconEnum.FLAME, recentlyAdded)}
+      {renderCard("Hot Cryptos", IconEnum.HOURGLASS, recentlyAdded)}
     </div>
   );
 };
